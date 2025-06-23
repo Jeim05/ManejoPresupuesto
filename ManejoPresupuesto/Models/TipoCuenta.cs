15a9e7b57907839764c16783504ff86e95b5b272
Jeim05 <lopezvegajeimmy@gmail.com>
2025-06-22 21:42:44 -0600

    Validaciones del modelo

--- ManejoPresupuesto/Models/TipoCuenta.cs
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -3,32 +3,29 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ManejoPresupuesto.Models
 {
-    public class TipoCuenta
+    public class TipoCuenta:IValidatableObject
     {
         public int IdTipoCuenta { get; set; }
 
         [Required(ErrorMessage = "El campo {0} es requerido")] // Esto se utiliza para validar que el campo no este vacio
-        [PrimeraLetraMayuscula]
+        //[PrimeraLetraMayuscula]
         public string Nombre { get; set; }
 
         public int UsuarioId { get; set; }
 
         public int Orden { get; set; }
 
-        /* PRUEBAS DE OTRAS VALIDACIONES POR DEFECTO 
-        [Required(ErrorMessage = "El campo {0} es requerido")]
-        [EmailAddress(ErrorMessage = "El campo debe ser un correo electrónico valido")]
-        public string Email { get; set; }
+       public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(Nombre !=null && Nombre.Length > 0)
+            {
+                var primeraLetra = Nombre[0].ToString();
 
-        [Range(minimum:18, maximum:130, ErrorMessage ="El valor de la {0} debe estar entre {1} y {2}")]
-        public int Edad { get; set; }
-
-        [Url(ErrorMessage = "El campo debe ser una url valida")]
-        public string URL { get; set; }
-
-        [CreditCard(ErrorMessage ="La tarjeta de crédito no es valida")]
-        [Display(Name = "Tarjeta de crédito") ]
-        public string TarjetaDeCredito { get; set; }
-        */
+                if (primeraLetra != primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe ser mayuscula", new[] { nameof(Nombre) });
+                }
+            }
+        }
     }
 }
