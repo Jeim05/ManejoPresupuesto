284085c4931e5c57cca91ccfb5d44e01712c242e
Jeim05 <lopezvegajeimmy@gmail.com>
2025-06-22 21:43:57 -0600

    Validaciones

--- ManejoPresupuesto/Models/TipoCuenta.cs
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -3,29 +3,29 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ManejoPresupuesto.Models
 {
-    public class TipoCuenta:IValidatableObject
+    public class TipoCuenta
     {
         public int IdTipoCuenta { get; set; }
 
         [Required(ErrorMessage = "El campo {0} es requerido")] // Esto se utiliza para validar que el campo no este vacio
-        //[PrimeraLetraMayuscula]
+        [PrimeraLetraMayuscula]
         public string Nombre { get; set; }
 
         public int UsuarioId { get; set; }
 
         public int Orden { get; set; }
 
-       public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if(Nombre !=null && Nombre.Length > 0)
-            {
-                var primeraLetra = Nombre[0].ToString();
+       //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+       // {
+       //     if(Nombre !=null && Nombre.Length > 0)
+       //     {
+       //         var primeraLetra = Nombre[0].ToString();
 
-                if (primeraLetra != primeraLetra.ToUpper())
-                {
-                    yield return new ValidationResult("La primera letra debe ser mayuscula", new[] { nameof(Nombre) });
-                }
-            }
-        }
+       //         if (primeraLetra != primeraLetra.ToUpper())
+       //         {
+       //             yield return new ValidationResult("La primera letra debe ser mayuscula", new[] { nameof(Nombre) });
+       //         }
+       //     }
+       // }
     }
 }
