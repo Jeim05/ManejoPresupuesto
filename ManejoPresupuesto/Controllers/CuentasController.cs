6ff03b9a229bd2ac70550f2959c8aa6c8b88d422
Jeim05 <lopezvegajeimmy@gmail.com>
2025-07-05 19:56:46 -0600

    Formulario de cuentas

---
+++ ManejoPresupuesto/Controllers/CuentasController.cs
@@ -0,0 +1,31 @@
+﻿using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Threading.Tasks;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class CuentasController : Controller
+    {
+        private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
+        private readonly IServicioUsuarios servicioUsuarios;
+
+        public CuentasController(IRepositorioTiposCuentas repositorioTiposCuentas,
+                                 IServicioUsuarios servicioUsuarios)
+        {
+            this.repositorioTiposCuentas = repositorioTiposCuentas;
+            this.servicioUsuarios = servicioUsuarios;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Crear()
+        {
+            var usuarioId = servicioUsuarios.obtenerUsuarioId();
+            var tiposCuentas = await repositorioTiposCuentas.Obtener(usuarioId);
+            var modelo = new CuentaCreacionViewModel();
+            modelo.TiposCuentas = tiposCuentas.Select(x => new SelectListItem(x.Nombre, x.IdTipoCuenta.ToString()));
+            return View(modelo);
+        }
+    }
+}
---
+++ ManejoPresupuesto/Models/CuentaCreacionViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManejoPresupuesto.Models
+{
+    public class CuentaCreacionViewModel
+    {
+        //SelectListItem es una opción de asp net core que permite crear select de una manera muy sencilla
+        public IEnumerable<SelectListItem> TiposCuentas { get; set; }
+    }
+}
